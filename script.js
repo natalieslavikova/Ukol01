157681d8b4059030392f3152b3862db1849ace7e
Natálie Slavíková <slavikova.natalie@centrum.cz>
2024-02-07 20:48:00 +0100

    final

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ Pokud si ovšem neuvědomíte, že Američané používají pro měření teplot
 
 Nastudujte si na České Wikipedii jak se převádějí stupně Fahrenheita na stupně Celsia a vytvořte webovou stránku, která takový převod provede.
  Vaše stránka se zeptá uživatele teplotu ve stupních Fahrenheita a pomocí document.body.innerHTML vypíše její ekvivalent ve stupních Celsia.*/
+
+ const fahrnheit = Number(prompt("Zadej stupně Fahrnheita"))
+
+ const prevod= 5 * (fahrnheit - 32) / 9
+
+ document.body.innerHTML += `<h2> ${fahrnheit} stupňů Fahrenheita je ${prevod} stupňů Celsia</h2>`;
